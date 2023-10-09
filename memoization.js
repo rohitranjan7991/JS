f48e836cef4c8dc72d845210c0b3c0c12c8674e2
ROHIT <rohitranjan7991@gmail.com>
2023-10-09 08:12:12 +0530

    memoization done

--- memoization.js
+++ memoization.js
@@ -1,2 +1,35 @@
 // Memoization is technique that can be used to reduce time-consuming calculations
 // by saving previous inputs to something called cache and reeturning the result from it.
+
+let sum = 0;
+const calc = (n) => {
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+const memoize = (func) => {
+  const cache = {};
+  return function (...args) {
+    const num = args[0];
+    if (num in cache) {
+      console.log("value from cache");
+      return cache[num];
+    } else {
+      console.log("from non cache");
+      const result = func(num);
+      cache[num] = result;
+      return result;
+    }
+  };
+};
+
+console.time();
+const val = memoize(calc)(5);
+console.log(val);
+console.timeEnd();
+
+console.time();
+console.log(val);
+console.timeEnd();
